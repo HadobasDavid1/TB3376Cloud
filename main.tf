d444266ca843dabbe40dd9c1d0b7ff7c9ee6097c
HadobasDavid1 <91009606+HadobasDavid1@users.noreply.github.com>
2023-05-16 13:45:09 +0200

    Update main.tf - Mama

--- main.tf
+++ main.tf
@@ -12,19 +12,20 @@ module "read" {
 output "final_output" {
   value = module.read.read_output
 }
-# locals {
-#   answers = read("modules/write/answers.tfvars")
-# }
-# 
-# module "write" {
-#   source = "./modules/write"
-# 
-#   answer_1 = local.answers["answer_1"]
-#   answer_2 = local.answers["answer_2"]
-#   answer_3 = local.answers["answer_3"]
-#   answer_4 = local.answers["answer_4"]
-#   answer_5 = local.answers["answer_5"]
-# }
+# DO NOT DELETE ANYTHING BEFORE THIS POINT
+ locals {
+   answers = read("modules/write/answers.tfvars")
+ }
+ 
+ module "write" {
+   source = "./modules/write"
+ 
+   answer_1 = local.answers["answer_1"]
+   answer_2 = local.answers["answer_2"]
+   answer_3 = local.answers["answer_3"]
+   answer_4 = local.answers["answer_4"]
+   answer_5 = local.answers["answer_5"]
+ }
 # 
 # module "data" {
 #   source = "./modules/data"
