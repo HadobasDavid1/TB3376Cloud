d4355a4b87fa340c891a0bbda144cb0d65863ccd
HadobasDavid1 <91009606+HadobasDavid1@users.noreply.github.com>
2023-05-16 12:51:44 +0200

    Update main.tf - Is this the real life?

--- main.tf
+++ main.tf
@@ -10,17 +10,17 @@ module "read" {
 }
 
 locals {
-  answers = jsondecode(file("./modules/write/answers.tfvars"))
+  answers = read("modules/write/answers.tfvars")
 }
 
 module "write" {
   source = "./modules/write"
 
-  answer_1 = local.answers.answer_1
-  answer_2 = local.answers.answer_2
-  answer_3 = local.answers.answer_3
-  answer_4 = local.answers.answer_4
-  answer_5 = local.answers.answer_5
+  answer_1 = local.answers["answer_1"]
+  answer_2 = local.answers["answer_2"]
+  answer_3 = local.answers["answer_3"]
+  answer_4 = local.answers["answer_4"]
+  answer_5 = local.answers["answer_5"]
 }
 
 module "data" {
