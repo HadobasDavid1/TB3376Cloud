4a7da85269149d3281501b6eb5e6537fd1f56974
HadobasDavid1 <91009606+HadobasDavid1@users.noreply.github.com>
2023-05-16 12:56:14 +0200

    Update main.tf - please just let me see a green checkmark, for the love of God

--- main.tf
+++ main.tf
@@ -8,34 +8,33 @@ module "read" {
   source          = "./modules/read"
   read_variable   = module.files.example_output
 }
-
-locals {
-  answers = read("modules/write/answers.tfvars")
-}
-
-module "write" {
-  source = "./modules/write"
-
-  answer_1 = local.answers["answer_1"]
-  answer_2 = local.answers["answer_2"]
-  answer_3 = local.answers["answer_3"]
-  answer_4 = local.answers["answer_4"]
-  answer_5 = local.answers["answer_5"]
-}
-
-module "data" {
-  source = "./modules/data"
-  file_path = module.files.file_path
-}
-
-output "read_file_content" {
-  value = module.read.file_content
-}
-
-output "write_answers" {
-  value = module.write.answers
-}
-
-output "file_id" {
-  value = module.data.file_id
-}
+# locals {
+#   answers = read("modules/write/answers.tfvars")
+# }
+# 
+# module "write" {
+#   source = "./modules/write"
+# 
+#   answer_1 = local.answers["answer_1"]
+#   answer_2 = local.answers["answer_2"]
+#   answer_3 = local.answers["answer_3"]
+#   answer_4 = local.answers["answer_4"]
+#   answer_5 = local.answers["answer_5"]
+# }
+# 
+# module "data" {
+#   source = "./modules/data"
+#   file_path = module.files.file_path
+# }
+# 
+# output "read_file_content" {
+#   value = module.read.file_content
+# }
+# 
+# output "write_answers" {
+#   value = module.write.answers
+# }
+# 
+# output "file_id" {
+#   value = module.data.file_id
+# }
