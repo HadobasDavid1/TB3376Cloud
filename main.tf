f0acb554fd8da919cfb09d1e7a014a55b9a2f6b3
HadobasDavid1 <91009606+HadobasDavid1@users.noreply.github.com>
2023-05-16 13:37:50 +0200

    Update main.tf - Just k!lled a man

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "files" {
 
 module "read" {
   source        = "./modules/read"
-  read_variable = module.files.example_output
+  read_variable = toString(module.files.example_output)
 }
 
 output "final_output" {
